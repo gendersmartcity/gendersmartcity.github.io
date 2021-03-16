0b45171632c5710c1bbfc3a18bcd421c4d678e0f
prtysh <prtyshrmn@gmail.com>
2021-03-16 22:56:30 +0530

    final commits

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:prtysh/gsccopy.git master:gh-pages
+git push -f git@github.com:gendersmartcity/gendersmartcity.github.io.git master:gh-pages
 cd -
